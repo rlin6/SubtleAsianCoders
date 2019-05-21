3a6fad2fc0d92a85c4d1d7996bd68def5b06b2cc
Kyle Tau <ktau@stuy.edu>
2019-05-21 12:12:14 -0400

    added small part of api

---
+++ api/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+elements = [
+    {
+        "name": "Hydrogen",
+        "symbol": "H",
+        "atomic number": 1,
+        "atomic mass": 1.00794
+    },
+]
+
+class Element(Resource):
+    def get(self, name):
+        for e in elements:
+            if(name == e["name"]):
+                return e, 200
+        return "Element not found", 404
+
+api.add_resource(Element, "/elements/<string:name>")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
