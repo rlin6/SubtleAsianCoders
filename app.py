da575e41d6c9c7fac8c9b6247bb2b9191513f452
ccai1 <ccai1@stuy.edu>
2019-05-29 11:57:55 -0400

    p

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__, template_folder='templates')
 def chem():
     return render_template('index.html')
 
+@app.route("/physics")
+def physics():
+    return render_template('physics.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
