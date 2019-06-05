38d2217d5fdfcfb3dd41282c3571b11178a4a457
cliu5 <cliu5@stuy.edu>
2019-06-04 22:59:37 -0400

    done

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, render_template
 app = Flask(__name__, template_folder='templates', static_url_path='')
+import elements as chempy
 
 @app.route("/")
-def chem():
+def home():
     return render_template('index.html')
+
+@app.route("/chem")
+def chemistry():
+    return render_template('chemistry.html')
 
 @app.route("/physics")
 def physics():
     return render_template('physics.html')
-
+'---------------------------------  PHYSICS  -----------------------'
 @app.route("/rope")
 def rope():
     return app.send_static_file('rope.html')
@@ -16,6 +21,29 @@ def rope():
 @app.route("/coulomb")
 def cou():
     return app.send_static_file('coulomb.html')
+
+'---------------------------------  CHEMISTRY -----------------------'
+
+@app.route("/receive", methods=['GET','POST'])
+def choose():
+    elementList=request.form.getlist('type')
+    displayStr=''
+    for i in elementList:
+        print(i)
+        displayStr+=i
+        displayStr+=', '
+        
+    massList=[chempy.getMass(i) for i in elementList]
+    electroList=[chempy.getElectro(i) for i in elementList]
+    radiusList=[chempy.getRadius(i) for i in elementList]
+    stateList=[chempy.getState(i) for i in elementList]
+    yearList=[chempy.getYear(i) for i in elementList]
+    blurbList=[chempy.getBlurb(i) for i in elementList]
+    
+    return render_template('chosen.html',eList=elementList, dStr=displayStr, mList=massList,
+                           electroList=electroList, rList=radiusList, sList=stateList,
+                           yList=yearList,bList=blurbList)
+
 
 if __name__ == "__main__":
     app.debug = True
---
+++ elements.py
@@ -0,0 +1,42 @@
+import json
+
+with open('elements.json','r') as f:
+    elements_dict=json.load(f)
+        
+def getElement(symbol):
+    for element in elements_dict:
+        if element['symbol']==symbol:
+            return element
+        
+def getMass(element):
+    for e in elements_dict:
+        if e['symbol']==element:
+            return e['mass']
+
+def getElectro(element):
+    for e in elements_dict:
+        if e['symbol']==element:
+            return e['electronegativity']
+        
+
+def getRadius(element):
+    for e in elements_dict:
+        if e['symbol']==element:
+            return e['atomicRadius']
+
+def getState(element):
+    for e in elements_dict:
+        if e['symbol']==element:
+            return e['state']
+
+def getYear(element):
+    for e in elements_dict:
+        if e['symbol']==element:
+            return e['year']
+        
+def getBlurb(element):
+    for e in elements_dict:
+        if e['symbol']==element:
+            return e['blurb']
+#print(getElement('H'))
+#print(getMass('Hydrogen'))
