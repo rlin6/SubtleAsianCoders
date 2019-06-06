dfcc7bb8d4466ecbb1f6649d83a1a5666e2a0dd8
ccai1 <ccai1@stuy.edu>
2019-06-06 14:22:42 -0400

    more electirc field

--- static/js/physics.js
+++ static/js/physics.js
@@ -10,6 +10,7 @@ var renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.domElement.id = 'canv';
 var charge = 0;
+var clicked = 0;
 document.getElementById("playground").appendChild( renderer.domElement );
 document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 
@@ -35,9 +36,9 @@ var arrows = [];
 for (let x = 0; x < dimensions + 1; x+=2) {
   for (let y = 0; y < dimensions + 1; y+=2) {
     for (let z = 0; z < dimensions + 1; z+=2) {
-      var direction = new THREE.Vector3(THREE.Math.randFloatSpread(2), THREE.Math.randFloatSpread(2), THREE.Math.randFloatSpread(2)).normalize();
+      var d = new THREE.Vector3(THREE.Math.randFloatSpread(2), THREE.Math.randFloatSpread(2), THREE.Math.randFloatSpread(2)).normalize();
       var ori = new THREE.Vector3(x - dimensions/2, y - dimensions/2, z - dimensions/2);
-      var arrow = new THREE.ArrowHelper(direction, ori, 1,  0x800080, 0.10, 0.2, 0.02);
+      var arrow = new THREE.ArrowHelper(d, ori, 1,  0x800080, 0.10, 0.2, 0.02);
       arrows.push(arrow);
       scene.add(arrow);
     }
@@ -48,11 +49,10 @@ console.log(arrows);
 // charges
 var charges = [];
 chargePositive.addEventListener("click", function() {
-  charge = 1;
+  clicked = 1;
 });
 chargeNegative.addEventListener("click", function() {
-  charge = -1;
-  console.log(clicked)
+  clicked = -1;
 });
 //arrange.addEventListener("click", arrangeArrows);
 
@@ -61,11 +61,11 @@ function onDocumentMouseDown( event ) {
 
   // event.preventDefault();
 
-  if (charge != 0) {
+  if (clicked != 0) {
 
-    cgeometry = new THREE.SphereGeometry(0.25, 16, 12);
+    cgeometry = new THREE.SphereGeometry(0.1, 16, 16);
     charge_material = new THREE.MeshBasicMaterial({
-      color: charge == 1 ? 0xff0000 : 0x0000ff
+      color: clicked == 1 ? 0xba0000 : 0x4422ee
     });
 
     charge = new THREE.Mesh(cgeometry, charge_material);
@@ -88,7 +88,7 @@ function onDocumentMouseDown( event ) {
             }
 
     charge.z = getZ().z;
-    console.log(charge.z)
+    console.log(charge.z);
     // console.log(charge.z)
     // console.log(charge.x)
     // mouse.unproject( camera );
@@ -98,12 +98,11 @@ function onDocumentMouseDown( event ) {
     // raycaster.setFromCamera( mouse.clone(), camera );
     // var objects = raycaster.intersectObjects(scene.children);
     // console.log(objects)
-    setCharge(charge)
-    charge = 0
+    setCharge(clicked);
+    clicked = 0;
   }
 
 }
-
 
 
 function setCharge(val) {
@@ -178,7 +177,40 @@ pos.setXYZ( 5, -dimensions/2, -dimensions/2, -dimensions/2 );
 // text.rotation = camera.rotation;
 // scene.add(text);
 //
+
+// var loader = new THREE.FontLoader();
+// var text_geo;
+//
+// loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+//
+// 	text_geo = new THREE.TextGeometry( 'X', {
+// 		font: font,
+//     size: 16,
+//     height: 2,
+//     curveSegments: 6,
+//     style: "normal"
+// 	} );
+// } );
+//
+// var  color = new THREE.Color();
+// color.setRGB(255, 250, 250);
+// var  textMaterial = new THREE.MeshBasicMaterial({ color: color });
+// var  text = new THREE.Mesh(text_geo , textMaterial);
+//
+// // console.log( axes.geometry.attributes.position)
+//
+// // text.position.x = axes.geometry.attributes.position[0];
+// // text.position.y = axes.geometry.attributes.position[1];
+// // text.position.z = axes.geometry.attributes.position[2];
+// text.position.x = -4;
+// text.position.y = -4;
+// text.position.z = -4;
+//
+// text.rotation = camera.rotation;
+//
+// scene.add( text );
 scene.add( axes );
+
 
 render();
 function render() {
