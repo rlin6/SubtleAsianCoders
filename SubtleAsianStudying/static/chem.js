1a9ebe75b7758c30ac313360df1e4ab5ee74f86b
Kyle Tau <kyletau2@gmail.com>
2019-06-06 20:02:38 -0400

    clear and submit list

--- SubtleAsianStudying/static/chem.js
+++ SubtleAsianStudying/static/chem.js
@@ -2,6 +2,16 @@ function addItem(x){
         //console.log("hi")
         //console.log(x)
         y = document.getElementById("chosen").innerHTML;
-        console.log(y)
-        document.getElementById("chosen").innerHTML = x + ", " + y
+        //console.log(y)
+        if (x == 'empty') {
+          document.getElementById("chosen").innerHTML = "";
+        }
+        else if (y != "") {
+          document.getElementById("chosen").innerHTML = y + ", " + x
+        }
+        else {
+          document.getElementById("chosen").innerHTML = x;
+        }
+        var list = document.getElementById("submit");
+        list.value = document.getElementById("chosen").innerHTML;
     }
